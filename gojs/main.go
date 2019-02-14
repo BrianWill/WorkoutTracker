1fe85a1244f7e6390944dc3f848850b0b81c946c
Brian Will <brian.thomas.will@gmail.com>
2019-02-14 02:56:51 -0800

    basic server set up to persist data in file

--- gojs/main.go
+++ gojs/main.go
@@ -4,6 +4,35 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
+type User struct {
+	Name string
+	// todo how is int64 represented in JS?
+	Workouts  map[int64]*Workout // indexed by StartTime
+	Templates []Workout          // user's personal list of templates
+	Exercises []Exercise         // user's personal list of exercises (SetsExpected used but Sets is empty)
+}
+
+type Workout struct {
+	Name      string
+	StartTime int64 // unix time
+	EndTime   int64 // unix time
+	Exercises []Exercise
+}
+
+type Exercise struct {
+	Name     string
+	Sets     []Set // the actual sets performed by the user
+	Expected []Set // parallel (the values expected for the user to perform)
+	Notes    string
+}
+
+type Set struct {
+	Reps     int
+	Weight   int
+	Duration int // time in milliseconds of time to perform set
+	Rest     int // time in milliseconds of rest before next exercise
+}
+
 func main() {
 	doc := js.Global.Get("document")
 	println("Hello, browser console!", doc)
--- main.go
+++ main.go
@@ -1,18 +1,116 @@
 package main
 
 import (
-	"database/sql"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 	_ "github.com/lib/pq"
+	uuid "github.com/satori/go.uuid"
 )
+
+const filePath = "userData.dat"
+
+type User struct {
+	Name string
+	// todo how is int64 represented in JS?
+	Workouts  map[int64]*Workout // indexed by StartTime
+	Templates []Workout          // user's personal list of templates
+	Exercises []Exercise         // user's personal list of exercises (SetsExpected used but Sets is empty)
+}
+
+type Workout struct {
+	Name      string
+	StartTime int64 // unix time
+	EndTime   int64 // unix time
+	Exercises []Exercise
+}
+
+type Exercise struct {
+	Name     string
+	Sets     []Set // the actual sets performed by the user
+	Expected []Set // parallel (the values expected for the user to perform)
+	Notes    string
+}
+
+type Set struct {
+	Reps     int
+	Weight   int
+	Duration int // time in milliseconds of time to perform set
+	Rest     int // time in milliseconds of rest before next exercise
+}
+
+type UserMap struct {
+	sync.RWMutex
+	internal map[string]string
+}
+
+func NewUserMap() *UserMap {
+	return &UserMap{
+		internal: make(map[string]string),
+	}
+}
+
+func (um *UserMap) Exists(key string) bool {
+	um.RLock()
+	_, ok := um.internal[key]
+	um.RUnlock()
+	return ok
+}
+
+func (um *UserMap) Delete(key string) {
+	um.Lock()
+	delete(um.internal, key)
+	um.Unlock()
+}
+
+func (um *UserMap) Store(userID string, data string) {
+	um.Lock()
+	um.internal[userID] = data
+	um.Unlock()
+}
+
+func initUser() User {
+	return User{}
+}
+
+func Load() (*UserMap, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	users := &UserMap{}
+	return users, json.NewDecoder(f).Decode(users)
+}
+
+func Save(users *UserMap) error {
+	f, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	b, err := json.Marshal(users)
+	if err != nil {
+		return err
+	}
+	r := bytes.NewReader(b)
+	_, err = io.Copy(f, r)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 
@@ -24,10 +122,26 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	// db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	// if err != nil {
+	// 	log.Fatalf("Error opening database: %q", err)
+	// }
+
+	users, err := Load()
 	if err != nil {
-		log.Fatalf("Error opening database: %q", err)
+		users = NewUserMap()
 	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		// wait for any pending requests to finish
+		fmt.Println(" SIGINT received")s
+		users.Lock()
+		defer users.Unlock()
+		os.Exit(0)
+	}()
 
 	router := gin.New()
 	router.Use(gin.Logger())
@@ -40,36 +154,78 @@ func main() {
 		c.HTML(http.StatusOK, "home.tmpl", nil)
 	})
 
-	router.GET("/tick", func(c *gin.Context) {
-		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error creating database table: %q", err))
-			return
-		}
-
-		if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error incrementing tick: %q", err))
-			return
-		}
-
-		rows, err := db.Query("SELECT tick FROM ticks")
+	router.GET("/signup", func(c *gin.Context) {
+		userID, err := c.Cookie("user_id")
 		if err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error reading ticks: %q", err))
+			c.String(http.StatusBadRequest, "Invalid cookie.")
+		}
+		users.Lock()
+		defer users.Unlock()
+		if _, ok := users.internal[userID]; ok {
+			c.String(http.StatusOK, "you already have a user id: "+userID)
+			return
+		}
+		u2 := uuid.NewV4()
+		userID = u2.String()
+		const tenYears = 10 * 365 * 24 * 60 * 60
+		c.SetCookie("user_id", userID, tenYears, "/", "", false, false)
+		bytes, err := json.Marshal(initUser())
+		if err != nil {
+			fmt.Printf("Error JSON encoding state: %+v", err)
+		}
+		users.internal[userID] = string(bytes)
+		c.String(http.StatusOK, "new user created with id: "+userID)
+	})
+
+	// save all of user's data
+	router.POST("/store", func(c *gin.Context) {
+		userID, err := c.Cookie("user_id")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Invalid cookie.")
+		}
+		payload := c.PostForm("payload")
+
+		// verify the data is valid
+		var data *User
+		err = json.Unmarshal([]byte(payload), data)
+		if err != nil {
+			fmt.Printf("error decoding JSON: %s", err)
+			c.String(http.StatusBadRequest, "Error in decoding JSON payload.")
 			return
 		}
 
-		defer rows.Close()
-		for rows.Next() {
-			var tick time.Time
-			if err := rows.Scan(&tick); err != nil {
-				c.String(http.StatusInternalServerError,
-					fmt.Sprintf("Error scanning ticks: %q", err))
-				return
-			}
-			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+		users.Lock()
+		defer users.Unlock()
+		_, ok := users.internal[userID]
+		if !ok {
+			c.String(http.StatusUnauthorized, "You are not a known user.")
+			users.Unlock()
+			return
 		}
+		users.internal[userID] = payload
+
+		err = Save(users)
+		if err != nil {
+			fmt.Println("error saving user map: " + err.Error())
+		}
+
+		c.String(http.StatusOK, "Saved data.")
+	})
+
+	// get all of user's data
+	router.POST("/load", func(c *gin.Context) {
+		userID, err := c.Cookie("user_id")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Invalid cookie.")
+		}
+		users.Lock()
+		defer users.Unlock()
+		data, ok := users.internal[userID]
+		if !ok {
+			c.String(http.StatusUnauthorized, "You are not a known user.")
+			return
+		}
+		c.JSON(http.StatusOK, data)
 	})
 
 	router.Run(":" + port)
